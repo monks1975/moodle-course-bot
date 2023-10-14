80f66f17d83d2cf60c15098f6663080f74026b1f
monks1975 <mikmakmok@gmail.com>
2023-10-14 10:28:00 +0100

    v

--- moodle/block_openai_chat/version.php
+++ moodle/block_openai_chat/version.php
@@ -2,6 +2,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023042466;
+$plugin->version = 2023042467;
 $plugin->requires = 2023041800; // Requires this Moodle version.
 $plugin->component = 'block_openai_chat'; // Full name of the plugin (used for diagnostics)
