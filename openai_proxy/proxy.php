cd3e98961d40bd4dc308f654dba85086e743af49
monks1975 <mikmakmok@gmail.com>
2023-10-14 09:33:25 +0100

    amended openai proxy

--- openai_proxy/proxy.php
+++ openai_proxy/proxy.php
@@ -1,37 +1,33 @@
 <?php
-// Define the OpenAI API key and endpoint
-$openaiApiKey = "your-openai-api-key-here";
-$openaiEndpoint = "https://api.openai.com";
 
-// Capture the incoming request payload and headers
+// Load API key from file
+$apiKeyFilePath = '/api_key.txt';
+$apiKey = trim(file_get_contents($apiKeyFilePath));
+
+// Get the path from the incoming request and sanitize it
+$requestPath = ltrim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
+
+// Initialize cURL session
+$ch = curl_init("https://api.openai.com/{$requestPath}");
+
+// Read incoming request payload
 $incomingPayload = file_get_contents('php://input');
-$headers = getallheaders();
-
-// Remove any Authorization header that might have come from the client
-unset($headers['Authorization']);
-
-// Initialize a cURL session
-$ch = curl_init();
-
-// Create a proper URL based on the incoming request
-$requestUri = $_SERVER['REQUEST_URI'];
-$forwardingUrl = $openaiEndpoint . str_replace("/your-proxy-base-url", "", $requestUri);
 
 // Set cURL options
-curl_setopt($ch, CURLOPT_URL, $forwardingUrl);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $_SERVER['REQUEST_METHOD']);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $incomingPayload);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $_SERVER['REQUEST_METHOD']); // Pass through the HTTP method (GET, POST, etc.)
+curl_setopt($ch, CURLOPT_POSTFIELDS, $incomingPayload);
 curl_setopt($ch, CURLOPT_HTTPHEADER, [
-    "Authorization: Bearer " . $openaiApiKey,
-    "Content-Type: " . $headers["Content-Type"],
+    'Content-Type: application/json',
+    'Authorization: Bearer ' . $apiKey,
 ]);
 
-// Execute the cURL request and capture the response
+// Execute and capture the response
 $response = curl_exec($ch);
 
-// Forward the cURL response back to the client
-echo $response;
-
-// Close the cURL session
+// Close cURL session
 curl_close($ch);
+
+// Send the response back to the client
+header('Content-Type: application/json');
+echo $response;
